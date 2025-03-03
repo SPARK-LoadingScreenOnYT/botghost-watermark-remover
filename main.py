6a174aab93a2cb11602b12ef0e29d0cd419312db
Ethan Singleton <therealloadingscreenyt@gmail.com>
2025-03-02 17:51:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    await bot.change_presence(activity=discord.Streaming(name='streaming-name', url='https://www.twitch.tv/urtwitchusername'))
+    await bot.change_presence(activity=discord.Watching(name='Evergreen City Roleplay', url='https://www.twitch.tv/urtwitchusername'))
 
-bot.run('YOUR_BOT_TOKEN')
+bot.run('MTI2Mjk2NzQ3NjI0OTk1NjM3Mw.G5w8sZ.iRoWSWFNk3-SBYsseUHD6OGsNAdTz_DQlnVGDo')
